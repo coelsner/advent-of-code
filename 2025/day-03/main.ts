2be2e151a0240ccadaeef8b093e961aba5898dac
Christian Oelsner <christian.oelsner@og1o.de>
2025-12-25 23:16:50 +0100

    feat: Updates day-03 to resolve second part

--- 2025/day-03/main.ts
+++ 2025/day-03/main.ts
@@ -7,58 +7,54 @@ const test_input = `987654321111111
 
 const real_input = await Deno.readTextFile("./day-03/input.txt");
 
-if (import.meta.main) {
-  const input = test_input;
+type Battery = {
+  index: number;
+  voltage: number;
+};
 
-  const results = [];
+function toBattery(value: string, index: number): Battery {
+  return { index, voltage: parseInt(value) };
+}
+
+function getMax(batteries: Battery[], index: number, count: number): Battery[] {
+  const sorted = batteries
+    .slice(index, batteries.length - count + 1)
+    .toSorted((a, b) => b.voltage - a.voltage);
+
+  const max = sorted[0];
+
+  if (count === 1) {
+    return [max];
+  }
+
+  if (count <= batteries.length) {
+    return [max, ...getMax(batteries, max.index + 1, count - 1)];
+  }
+
+  return batteries.slice(0);
+}
+
+if (import.meta.main) {
+  const input = real_input;
+
+  const results = [] as string[];
 
   for (const bank of input.split("\n")) {
-    console.debug("Bank: ", bank);
+    //console.debug("Bank: ", bank);
 
-    const digits = bank.split("").map((x) => parseInt(x));
-    const maxLeft = digits.slice(0, -1).reduce((max, x) => Math.max(max, x), 0);
-    const indexOfLeft = digits.indexOf(maxLeft);
+    const batteries = bank.split("").map(toBattery);
+    const values = getMax(batteries, 0, 12)
+      .map((b) => b.voltage)
+      .join("");
 
-    const max2 = digits
-      .slice(indexOfLeft + 1, digits.length)
-      .reduceRight((max, x) => Math.max(max, x), 0);
-
-    const test = getNumbers(digits, 12);
-
-    console.debug(
-      "Digits:",
-      digits.join(""),
-      digits.length,
-      "max1:",
-      maxLeft,
-      "max2:",
-      max2,
-      "test",
-      test.join(""),
-      test.length
-    );
-    results.push(`${maxLeft}${max2}`);
+    results.push(values);
   }
+
+  //console.debug("Results: ", results);
 
   const sum = results
     .map((x) => parseInt(x))
     .reduce((sum, curr) => sum + curr, 0);
 
   console.log(sum);
-}
-
-function getNumbers(numbers: number[], count: number): number[] {
-  const sorted = numbers.toSorted((a, b) => b - a).slice(0, count);
-  console.debug("Sorted:", sorted.join(""), sorted.length);
-
-  const result: number[] = [];
-
-  for (let xI = 0; xI < numbers.length; xI++) {
-    const sI = sorted.indexOf(numbers[xI]);
-    if (sI >= 0) {
-      result.push(numbers[xI]);
-    }
-  }
-
-  return result;
 }
