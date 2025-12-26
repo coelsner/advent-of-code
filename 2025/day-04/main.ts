1009f5f08b55c0c4710d513034dd9087160104e9
Christian Oelsner <christian.oelsner@og1o.de>
2025-12-26 23:45:16 +0100

    feat: Updates day-04 to solve part 2

--- 2025/day-04/main.ts
+++ 2025/day-04/main.ts
@@ -11,14 +11,30 @@ const test_input = `..@@.@@@@.
 
 const real_input = await Deno.readTextFile("./day-04/input.txt");
 
-class Warehouse {
-  constructor(
-    public readonly shelves: number[][],
-    public readonly width: number,
-    public readonly height: number
-  ) {}
+type Position = { x: number; y: number };
 
-  getAccessible(): { x: number; y: number }[] {
+class Warehouse {
+  public readonly width: number;
+  public readonly height: number;
+  public readonly shelves: number[][];
+
+  constructor(shelves: number[][]) {
+    this.height = shelves.length;
+    this.width = shelves[0].length;
+    this.shelves = shelves.map((r, rowIdx) => {
+      return r.map((c, colIdx) => {
+        if (c === 0) {
+          return 0;
+        }
+        const adjacents = getAdjacents(rowIdx, colIdx, this.height, this.width);
+        return (
+          adjacents.map((a) => shelves[a.x][a.y]).filter((v) => v).length + 1
+        );
+      });
+    });
+  }
+
+  getAccessible(): Position[] {
     return this.shelves.flatMap((r, rowIdx) => {
       return r.flatMap((c, colIdx) => {
         return c > 0 && c < 5 ? [{ x: rowIdx, y: colIdx }] : [];
@@ -26,48 +42,54 @@ class Warehouse {
     });
   }
 
+  remove(pos: Position[]): Warehouse {
+    const updated = this.shelves.map((r, rowIdx) => {
+      return r.map((c, colIdx) => {
+        return pos.some((v) => v.x === rowIdx && v.y === colIdx) ? 0 : c;
+      });
+    });
+
+    return new Warehouse(updated);
+  }
+
   toString(): string[] {
     return this.shelves.map((r) => {
-      return r.map((v) => (v > 1 ? "@" : ".")).join("");
+      return r
+        .map((v) => {
+          if (v === 0) return ".";
+          if (v > 1 && v < 5) return "x";
+          return "@";
+        })
+        .join("");
     });
   }
 }
 
 if (import.meta.main) {
-  const input = test_input;
-  const parsed: boolean[][] = parseInput(input.split("\n"));
+  const input = real_input;
+  const parsed: number[][] = parseInput(input.split("\n"));
 
-  const warehouse: Warehouse = createWarehouse(parsed);
+  let warehouse: Warehouse = new Warehouse(parsed);
+  let accessible = warehouse.getAccessible();
+  let total = 0;
+  while (accessible.length > 0) {
+    //console.debug(warehouse.toString().join("\n"));
+    console.log("Result:", accessible.length);
+    total += accessible.length;
 
-  console.debug(warehouse.toString().join("\n"));
+    warehouse = warehouse.remove(accessible);
+    accessible = warehouse.getAccessible();
+  }
 
-  const accessible = warehouse.getAccessible();
-  console.log("Result:", accessible.length);
+  console.log("Total:",total);
 }
 
-function parseInput(rows: string[]): boolean[][] {
+function parseInput(rows: string[]): number[][] {
   return rows.map((row) => {
     return row.split("").map((column) => {
-      return column === "@" ? true : false;
+      return column === "@" ? 1 : 0;
     });
   });
-}
-
-function createWarehouse(shelves: boolean[][]): Warehouse {
-  const height = shelves.length;
-  const width = shelves[0].length;
-
-  const result: number[][] = shelves.map((r, rowIdx) => {
-    return r.map((c, colIdx) => {
-      if (c === false) {
-        return 0;
-      }
-      const adjacents = getAdjacents(rowIdx, colIdx, height, width)
-      return adjacents.map((a) => shelves[a.x][a.y]).filter(v => v).length + 1
-    })
-  });
-
-  return new Warehouse(result, width, height);
 }
 
 function getAdjacents(
