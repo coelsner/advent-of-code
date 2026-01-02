9758464cd53798873b4e9abc685efec32d6cf708
Christian Oelsner <christian.oelsner@og1o.de>
2026-01-02 07:29:52 +0100

    feat: Trys to solve part 2 of day-06, but doesn't do

--- 2025/day-06/main.ts
+++ 2025/day-06/main.ts
@@ -1,17 +1,18 @@
 import * as streams from "@std/streams";
 
-async function test_input() {
-    return Promise.resolve(
-        ReadableStream.from([`123 328  51 64 
+function test_input(f: any) {
+  return Promise.resolve(
+    ReadableStream.from([`123 328  51 64 
  45 64  387 23 
   6 98  215 314
-*   +   *   +  `]).pipeThrough(new streams.TextLineStream()));
-} 
+*   +   *   +  `]).pipeThrough(new streams.TextLineStream()),
+  );
+}
 
-async function real_input(f: Deno.FsFile) {
-    return f.readable.pipeThrough(
-        new TextDecoderStream()
-    ).pipeThrough(new streams.TextLineStream());
+function real_input(f: Deno.FsFile) {
+  return f.readable.pipeThrough(
+    new TextDecoderStream(),
+  ).pipeThrough(new streams.TextLineStream());
 }
 
 class X {
@@ -28,45 +29,52 @@ class X {
     this.product *= BigInt(n);
   }
 
-  getResult(op: string) : bigint {
-    switch(op) {
-        case "*": return this.product;
-        case "+": return this.sum;
-        default: throw new Error(`Unkown "${op}" operator.`)
+  getResult(op: string): bigint {
+    switch (op.trim()) {
+      case "*":
+        return this.product;
+      case "+":
+        return this.sum;
+      default:
+        throw new Error(`Unkown "${op}" operator.`);
     }
   }
 }
 
 if (import.meta.main) {
-    using f = await Deno.open("./day-06/input.txt", {read: true});
-    
-  const input = await real_input(f);
+  using f = await Deno.open("./day-06/input.txt", { read: true });
 
-  let state: "init" | "numbers" = "init";
-
+  const input = await test_input(f);
+  
   const numbers: X[] = [];
 
   let result: bigint[] = [];
 
   for await (const data of input) {
-    const elements = data.split(/\s+/g).filter((v) => v.trim().length > 0);
-    switch (state) {
-      case "init":
-        elements.forEach((n) => numbers.push(new X(parseInt(n))));
-        state = "numbers";
-        break;
-      case "numbers":
-        if (elements.length !== numbers.length) {
-            throw new Error(`Elements size ${elements.length} unequal to numbers length ${numbers.length}`)
-        }
-        if (elements[0] === "*" || elements[0] === "+") {
-            result = elements.map((v, i) => numbers[i].getResult(v));
-        } else {
-            elements.forEach((n, i) => numbers[i].update(parseInt(n)))
-        }
-        break;
+    const elements = data.split(/([0-9 ]{3}) ?/g).filter((s) =>
+      s.trim().length !== 0
+    );
+
+    if (numbers.length === 0) {
+      console.log("Initializing numbers", elements.length);
+      elements.map((n) => new X(parseInt(n))).forEach((e) => numbers.push(e));
+      continue;
+    }
+
+    if (elements.length !== numbers.length) {
+      throw new Error(
+        `Elements size ${elements.length} unequal to numbers length ${numbers.length}`,
+      );
+    }
+
+    const first = elements[0].trim();
+
+    if (first === "*" || first === "+") {
+      result = elements.map((v, i) => numbers[i].getResult(v));
+    } else {
+      elements.forEach((n, i) => numbers[i].update(parseInt(n)));
     }
   }
 
-  console.log(numbers, result, result.reduce((p,c) => p+c));
+  console.log(numbers, result, result.reduce((p, c) => p + c));
 }
