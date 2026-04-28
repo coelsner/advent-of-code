72157f81c0ae8479dbf5dcb341272ffcb8093558
Christian Oelsner <christian.oelsner@og1o.de>
2026-04-28 14:22:54 +0200

    feat: Trying to solve part 02 of day-08 (WIP)

--- 2025/day-08/main.ts
+++ 2025/day-08/main.ts
@@ -1,8 +1,9 @@
 import { parseInput } from "./Box.ts";
 import { Circuit, computeCircuits } from "./Circuit.ts";
-import { computeDistances } from "./distances.ts";
+import { computeDistances, Distance } from "./distances.ts";
 
-const test_input = `162,817,812
+const test_input = {
+  data: `162,817,812
 57,618,57
 906,360,560
 592,479,940
@@ -21,45 +22,67 @@ const test_input = `162,817,812
 941,993,340
 862,61,35
 984,92,344
-425,690,689`;
+425,690,689`,
+  cycles: 10,
+};
 
-const real_input = await Deno.readTextFile(`./day-08/input.txt`);
+const real_input = {
+  data: await Deno.readTextFile(`./day-08/input.txt`),
+  cycles: 1000,
+};
+
+function joinCircuits(circuits: Circuit[], distances: Distance[]) {
+  return distances.reduce<Circuit[]>(
+    (prev: Circuit[], distance: Distance) => {
+      return computeCircuits(prev, distance);
+    },
+    circuits,
+  ).toSorted((a, b) => (b.boxes.size - a.boxes.size));
+}
 
 // Solution of part 1:
 // Circuits: 279 after 1000 cycles.
 // The product of the first 3 circuits = 123420
+function partOne(circuits: Circuit[], distances: Distance[]) {
+  const result = joinCircuits(circuits, distances);
+
+  console.log("----- Part 01 ------");
+  console.log("Circuits:", result.length, "after", distances.length, "cycles.");
+  //console.log(result.slice(0,3))
+
+  const sumOf = 3;
+  const product = result.slice(0, sumOf).reduce<number>(
+    (prev: number, circuit: Circuit) => (prev * circuit.boxes.size),
+    1,
+  );
+  console.log("The product of the first", sumOf, "circuits =", product);
+}
+
+// Solution of part 2:
+// TODO: Find the last two circuits needed to be connected
+// It's not necessarily the last two distances, since they are maybe already part of the main circuit
+function partTwo(circuits: Circuit[], distances: Distance[]) {
+  console.log("----- Part 02 ------");
+  const result = joinCircuits(circuits, distances.slice(0, -2));
+
+  console.log(result);
+  const last = distances.slice(-2).map((d) => Math.max(d.start.x, d.end.x));
+  console.log(last, last.reduce((p, c) => p * c));
+}
 
 if (import.meta.main) {
-  const input = {
-    data: real_input,
-    cycles: 1000,
-    sumOf: 3,
-  }
+  const input = real_input;
 
   console.debug("Start day-08");
 
   const boxes = parseInput(input.data);
   const distances = computeDistances(boxes);
 
-  let circuits: Circuit[] = boxes.map(b => new Circuit(b));
+  const circuits: Circuit[] = boxes.map((b) => new Circuit(b));
   //console.log(circuits)
-  for (let i = 0; i < input.cycles; i++) {
-    circuits = computeCircuits(circuits, distances[i]);
-  }
 
-  const result = circuits.toSorted((a,b) => (b.boxes.size - a.boxes.size));
-
-  console.log("-----------");
-  console.log("Circuits:", result.length, "after", input.cycles, "cycles.");
-  //console.log(result.slice(0,3))
-
-  let product = 1;
-  for (let i = 0; i < input.sumOf; i++) {
-    //console.log("Combining:", result[i], product)
-    product = product * result[i].boxes.size
-  }
-
-  console.log("The product of the first", input.sumOf, "circuits =", product);
+  partOne(circuits, distances.slice(0, input.cycles));
+  partTwo(circuits, distances);
 
   console.debug("End day-08");
 }
